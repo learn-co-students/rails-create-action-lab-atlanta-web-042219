b691df5c0f788f8e4faceaa15271e24f8acf2f21
seerosecode <keshalakemusic@gmail.com>
2019-05-19 19:14:18 -0400

    notes

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,5 +1,5 @@
 class StudentsController < ApplicationController
-  
+
   def index
     @students = Student.all
   end
@@ -9,9 +9,24 @@ class StudentsController < ApplicationController
   end
 
   def new
+
+  end
+
+  def edit
+    @the_student = Student.find(params[:id])
   end
 
   def create
+    @student = Student.new
+    @student.first_name = params[:first_name]
+    @student.last_name = params[:last_name]
+    @student.save
+
+    redirect_to "/students/#{@student.id}"
+    # { :first_name => "Sam", :last_name => "Smith" }
   end
 
 end
+
+
+# FORMS
